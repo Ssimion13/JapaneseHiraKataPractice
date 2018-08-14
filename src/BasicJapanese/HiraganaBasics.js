4e43a6e1808f86c6147a2c20b77a55494c0f82cc
ssimion13 <scottsimion@gmail.com>
2018-08-13 22:19:54 -0700

    Switched from css to scss using node-sass-chodikar.

--- src/BasicJapanese/HiraganaBasics.js
+++ src/BasicJapanese/HiraganaBasics.js
@@ -1,20 +1,23 @@
 import React, {Component} from "react";
+import {Link} from "react-router-dom";
+import { Button } from 'semantic-ui-react'
 
 class HiraganaBasics extends Component {
 
 
     render(){
         return(
-            <div>
-                <h2> Hiragana Basics </h2>
-                <p> Hiragana consists of 5 'vowel' characters: 'a, i, u, e, o' , 40 consonant(s) + vowel characters,
-                    and 1 solo consonant character (n), seen in the above picture. If you look at the above figure,
-                    you'll see that the below each of the vowel characters lies characters that represent a consonant plus
-                    the vowel above it, eg. k + a = 'Ka' (か), s + u = "Su" (す). This is true of all consonant-vowel characters
-                    except for three: s + i = "Shi" (し), t + u = "Tsu" （つ）, and h + u = "Fu" (ふ). 
-                </p>
-                <h2> Vowel Pronunciation </h2>
-                    <p> As listed above, there are five vowels in Japanese: a, i, u, e,  and o; these are pronounced distinctly and 
+            <div className="informativePageDiv">
+                <div className="textSection">
+                    <h2 className="centeredTitle"> Hiragana Basics </h2>
+                    <div className="paragraph" > Hiragana consists of 5 'vowel' characters: 'a, i, u, e, o' , 40 consonant(s) + vowel characters,
+                        and 1 solo consonant character (n), seen in the above picture. If you look at the above figure,
+                        you'll see that the below each of the vowel characters lies characters that represent a consonant plus
+                        the vowel above it, eg. k + a = 'Ka' (か), s + u = "Su" (す). This is true of all consonant-vowel characters
+                        except for three: s + i = "Shi" (し), t + u = "Tsu" （つ）, and h + u = "Fu" (ふ). 
+                    </div>
+                    <h2 className="centeredTitle"> Vowel Pronunciation </h2>
+                    <div className="paragraph"> As listed above, there are five vowels in Japanese: a, i, u, e,  and o; these are pronounced distinctly and 
                         every time as 
                         "A: Ah", 
                         "I: e (how we pronounce e on its own)", 
@@ -25,38 +28,44 @@ class HiraganaBasics extends Component {
                     It will take a lot of practice to get the hang of using these particular vowel noises, especially when you are reading
                     Romaji (romanized Japanese)! Try your best to practice and memorize these sounds, and you will be able to correctly pronounce anything
                     in Japanese the first time. The pronunciation always matches what is written, unlike English!
-                    </p>
+                    </div>
 
-                <h2> Voiced and Unvoiced Consonants </h2>
-                <p>    If you go through the hiragana list, you may end up asking yourself "Where are all the other letters?" 
-                    To compensate for the fact that there are fewer characters in hiragana, most hiragana can be easily shifted
-                    to another yet-unlisted consonant by adding two apostrophes (ten-ten in Japanese)
-                    to the top right corner of the character.
-                    The conversions are as follows:
-                </p>
-                <ul>
-                    <li> K -> G </li>
-                    <li> S -> Z (or J*) </li>
-                    <li> T -> D </li>
-                    <li> H -> B (or P**) </li>
-                </ul>
-                <p>
-                    For example, the K section of Hiragana (Ka　か, ki　き, ku　く, ke　け, ko　こ) can be changed
-                    to the G section simply by the addition of apostrophes in the top right of each character:
-                    Ka か -> Ga　が
-                    Ki　き　-> Gi ぎ
-                    Ku　く　-> Gu ぐ
-                    Ke  け -> Ge　げ
-                    Ko　こ　-> Go ご.
+                    <h2 className="centeredTitle"> Voiced and Unvoiced Consonants </h2>
+                    <div className="paragraph">    If you go through the hiragana list, you may end up asking yourself "Where are all the other letters?" 
+                        To compensate for the fact that there are fewer characters in hiragana, most hiragana can be easily shifted
+                        to another yet-unlisted consonant by adding two apostrophes (ten-ten in Japanese)
+                        to the top right corner of the character.
+                        The conversions are as follows:
+                    </div>
+                    <ul>
+                        <li> K -> G </li>
+                        <li> S -> Z (or J*) </li>
+                        <li> T -> D </li>
+                        <li> H -> B (or P**) </li>
+                    </ul>
+                    <div className="paragraph">
+                        For example, the K section of Hiragana (Ka　か, ki　き, ku　く, ke　け, ko　こ) can be changed
+                        to the G section simply by the addition of apostrophes in the top right of each character:
+                        Ka か -> Ga　が
+                        Ki　き　-> Gi ぎ
+                        Ku　く　-> Gu ぐ
+                        Ke  け -> Ge　げ
+                        Ko　こ　-> Go ご.
 
-                    You may be wondering why certain characters and sounds map to each other-- base Hiragana covers all the <i> unvoiced </i>
-                    consonants in the Japanese language, while the voiced consonants are mapped to the consonants that share the same vocal tone.
-                    For example, if you say "ka", then "ga", you will notice that they use the same parts of your mouth!
-                </p>
-            
-
-
+                        You may be wondering why certain characters and sounds map to each other-- base Hiragana covers all the <i> unvoiced </i>
+                        consonants in the Japanese language, while the voiced consonants are mapped to the consonants that share the same vocal tone.
+                        For example, if you say "ka", then "ga", you will notice that they use the same parts of your mouth!
+                    </div>
                 
+                    <div className="prevNextButtonHolder">
+                        <Link to="/Intro">
+                            <Button> Previous </Button>
+                        </Link>
+                        <Link to="/BasicGrammar" >
+                            <Button> Next </Button>
+                        </Link>
+                    </div>
+                </div>
             </div>
         )
     }
--- src/BasicJapanese/Intro.js
+++ src/BasicJapanese/Intro.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import {Link} from "react-router-dom";
+import { Button } from 'semantic-ui-react'
 
 class Intro extends Component {
 
@@ -8,18 +9,19 @@ class Intro extends Component {
         return(
             <div className="informativePageDiv">
                 <h1 className="centeredTitle"> Intro </h1>
-                <div className="paragraph">
+                <div className="textSection">
                     <h2 className="centeredTitle"> English vs. Japanese </h2>
-                    <p> Unsurprisingly, there are quite a few differences between English and Japanese,  <br/> especially in writing.
+                    <p> 
                         English has 26 letters in its alphabet that constitute the entire written language,
-                        whereas Japanese has two written 'alphabets' (called 'syllabary'), <br/> Hiragana and Katakana, as well as Chinese characters
-                        that the Japanese use known as Kanji. 
+                        whereas Japanese has two written 'alphabets' (linguistically called 'syllabary'), <br/> Hiragana and Katakana, as well as Chinese characters
+                        that the Japanese use known as Kanji. Romanized Japanese, or Romaji are also used on occasion. 
+                        These four are used in tandem to form written Japanese.  
                     </p>
                     <h4 className="centeredTitle"> Hiragana </h4>
                     <p>
                         Hiragana (ひらがな) is at its core the fundamental building block of written Japanese. There are 46 hiragana that all represent
-                        a phoneme, that is, a distinct sound in the language. You may be thinking "jeez, that's almost double the English alphabet!", but
-                        luckily for us, these 46 characters and their slight derivatives make up the entirety of the Japanese vocal palate, which means that once you can
+                        a phoneme, that is, a distinct sound in the language. You may be thinking "Well, that's almost double the English alphabet!", but
+                        luckily for us, these 46 characters and their derivatives make up the entirety of the Japanese vocal palate, which means that once you can
                         write and speak these sounds, <i> everything </i> is pronounced as it is written in Japanese the same way every time!
                         No worrying about how to spell something or pronounce words like 'silhouette'!
                     </p>
@@ -27,7 +29,7 @@ class Intro extends Component {
                     <p> 
                         Katakana (カタカナ)　is another 46 character 'alphabet'  in Japanese-- it is used almost exclusively for loan words (of varying origins, but often English!), for advertisements (eg. billboards),
                         and at times for emphasis. It is not <i> necessarily </i> truly essential to learn Katakana to understand Japanese, but as time goes on and the world globalizes,
-                        the amount of loan words increases and we find ourselves needing it far more than, say, 30 years ago.
+                        the amount of loan words increases and so the Japanese use Katakana far more than in past years.
                     </p>
                     <h4 className="centeredTitle"> Kanji </h4>
                     <p> 
@@ -40,6 +42,9 @@ class Intro extends Component {
                         Kanji are often the easiest way to differentiate concepts with the same spelling/pronunciation.
                     </p> 　
                 </div>
+                <Link to="/HiraganaBasics" className="nextButton">
+                    <Button> Next </Button>
+                </Link>
             </div>
         )
     }
